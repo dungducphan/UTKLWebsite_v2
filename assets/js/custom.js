071e0a49e9458f74810e9a5d9970c829cb3fda68
Dung Phan <brianp.dung@utexas.edu>
2017-08-27 22:36:05 -0500

    "Third commit"

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,8 +39,8 @@
             $(function () {
                 $.vegas('slideshow', {
                     backgrounds: [
-                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
-                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
+                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
+                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
                       { src: 'assets/img/3.jpg', fade: 1000, delay: 9000 },
                       { src: 'assets/img/4.jpg', fade: 1000, delay: 9000 },
                       { src: 'assets/img/5.jpg', fade: 1000, delay: 9000 },
@@ -49,7 +49,7 @@
                     ]
                 })('overlay', {
                     /** SLIDESHOW OVERLAY IMAGE **/
-                    src: 'assets/plugins/vegas/overlays/03.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+                    src: 'assets/plugins/vegas/overlays/11.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
                 });
 
             });
